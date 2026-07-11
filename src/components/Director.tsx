48265ef34f0f37b0e7672c50f4824436d000c184
AtomicXiB <vikramxib@email.com>
2026-07-11 13:26:31 +0530

    Sangha-Jyoti-Teacher

--- src/components/Director.tsx
+++ src/components/Director.tsx
@@ -26,7 +26,7 @@ const profiles = [
   {
     id: "bhante",
     title: "Meet Our Meditator • Vipassana Teacher",
-    image: "/images/Sangha-Jyoti.jpeg",
+    image: "/images/Sangha-Jyoti-Teacher.jpeg",
     name: "Bhante Sangha Jyoti",
     role: "Meditator • Vipassana Teacher",
     experience: "20+ Years Experience",
